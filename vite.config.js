08802a22014062b9060d86fc494691e17b405950
loudjine-dev <mereddefoulialoudjine@gmail.com>
2025-12-27 00:17:56 +0100

    fix vite config

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,5 @@
-<<<<<<< HEAD
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: '/Healthy-vision/',
-})
-=======
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-})
->>>>>>> 5a24deb5d144256360e1762565ad92f685a3d9de
+  base: "/Healthy-vision/",
+});
